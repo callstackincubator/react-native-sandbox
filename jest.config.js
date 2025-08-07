fb33917b706c02a7916749518547c9d5233e8c0a
Aliaksandr Babrykovich <aliaksandr.babrykovich@callstack.com>
2025-08-08 00:31:55 +0200

    chore: run tests on CI

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
+/** @type {import('jest').Config} */
 module.exports = {
-  preset: 'react-native',
+  projects: [
+    '<rootDir>/apps/*/jest.config.js', // This will find all jest.config.js files in app directories
+  ],
+  // Common settings for all projects
   transformIgnorePatterns: [
     'node_modules/(?!(react-native|@react-native|@react-native-community|@callstack/react-native-sandbox|react-native-toast-message|react-native-reanimated)/)',
   ],
-  setupFilesAfterEnv: ['<rootDir>/../../jest.setup.js'],
 }
