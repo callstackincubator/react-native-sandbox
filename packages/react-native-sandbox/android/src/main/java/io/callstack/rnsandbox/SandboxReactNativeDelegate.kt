fc5f1cf12cc9cfcd77194ce91f97b8ae4f0237cd
Ryan Karn <rkarn@amazon.com>
2026-04-14 21:15:58 -0700

    chore: missed space in comment form prev commit

--- packages/react-native-sandbox/android/src/main/java/io/callstack/rnsandbox/SandboxReactNativeDelegate.kt
+++ packages/react-native-sandbox/android/src/main/java/io/callstack/rnsandbox/SandboxReactNativeDelegate.kt
@@ -389,7 +389,7 @@ class SandboxReactNativeDelegate(
          * returns `this` in SandboxContextWrapper, that causes infinite recursion and a
          * StackOverflowError. Android 13+ fixed this in ContextWrapper by delegating to mBase 
          * directly. We mirror that fix here to support older platforms.
-         * /
+         */
         override fun registerComponentCallbacks(callback: android.content.ComponentCallbacks) {
             baseContext.applicationContext.registerComponentCallbacks(callback)
         }
