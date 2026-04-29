4f954ffa66988797c141e49933f56b912aada3ce
Alex Babrykovich <abobrikovich@gmail.com>
2026-04-29 09:30:42 +0200

    chore: remove trailing space
    Co-authored-by: Alex Babrykovich <abobrikovich@gmail.com>

--- packages/react-native-sandbox/android/src/main/java/io/callstack/rnsandbox/SandboxReactNativeDelegate.kt
+++ packages/react-native-sandbox/android/src/main/java/io/callstack/rnsandbox/SandboxReactNativeDelegate.kt
@@ -387,7 +387,7 @@ class SandboxReactNativeDelegate(
          * On Android 12 and below, Context.registerComponentCallbacks() delegates to
          * getApplicationContext().registerComponentCallbacks(). Since getApplicationContext()
          * returns `this` in SandboxContextWrapper, that causes infinite recursion and a
-         * StackOverflowError. Android 13+ fixed this in ContextWrapper by delegating to mBase 
+         * StackOverflowError. Android 13+ fixed this in ContextWrapper by delegating to mBase
          * directly. We mirror that fix here to support older platforms.
          */
         override fun registerComponentCallbacks(callback: android.content.ComponentCallbacks) {
